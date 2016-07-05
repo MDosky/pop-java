c05745ba8f0a5d78a464c0702bdea64c728a4724
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-05 16:34:09 -0700

    Start a threaded broker and a fake JM

--- workspace/popjava/src/popjava/service/JavaJobManagerMain.java
+++ workspace/popjava/src/popjava/service/JavaJobManagerMain.java
@@ -2,24 +2,24 @@ package popjava.service;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.util.concurrent.Executors;
 import popjava.PopJava;
 import popjava.baseobject.AccessPoint;
+import popjava.baseobject.POPAccessPoint;
 import popjava.broker.Broker;
 import popjava.combox.ComboxServer;
-import popjava.combox.ComboxServerSocket;
 import popjava.combox.ComboxSocketFactory;
 import popjava.serviceadapter.POPJobManager;
 import popjava.system.POPSystem;
 
 /**
  * Central JobManager for Java based applications
+ *
  * @author Dosky
  */
 public class JavaJobManagerMain implements Runnable {
-    
+
     ServerSocket jobServer;
-    
+
     public static void main(String[] args) throws IOException {
         JavaJobManagerMain main = new JavaJobManagerMain(args);
         main.start();
@@ -28,21 +28,25 @@ public class JavaJobManagerMain implements Runnable {
     public JavaJobManagerMain(String... args) throws IOException {
         //jobServer = new ServerSocket(POPJobManager.DEFAULT_PORT);
         POPSystem.initialize(args);
-        
-        ComboxSocketFactory csf = new ComboxSocketFactory();
-        AccessPoint accessPoint = new AccessPoint(AccessPoint.SOCKET_PROTOCOL, AccessPoint.DEFAULT_HOST, POPJobManager.DEFAULT_PORT);
-        ComboxServer cs = csf.createServerCombox(accessPoint, null, Broker.getBroker());
-        //JavaJobManager jobM;
-        //jobM = PopJava.newActive(JavaJobManager.class, (Object[]) args);
-        
+
+        new Thread(() -> {
+            ComboxSocketFactory csf = new ComboxSocketFactory();
+            AccessPoint accessPoint = new AccessPoint(AccessPoint.SOCKET_PROTOCOL, AccessPoint.DEFAULT_HOST, POPJobManager.DEFAULT_PORT);
+            ComboxServer cs = csf.createServerCombox(accessPoint, null, Broker.getBroker());
+        }).start();
+
+        POPAccessPoint pap = new POPAccessPoint(String.format("socket://%s://%s:%d", ComboxSocketFactory.PROTOCOL, AccessPoint.DEFAULT_HOST, POPJobManager.DEFAULT_PORT));
+        JavaJobManager jobM;
+        jobM = PopJava.newActive(JavaJobManager.class, pap);
+
         POPSystem.end();
     }
-    
+
     private void start() {
     }
 
     @Override
     public void run() {
-        
+
     }
 }
