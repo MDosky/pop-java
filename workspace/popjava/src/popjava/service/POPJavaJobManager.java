efa85f8652580522a5c4e9d5f4e00c05fec11efb
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-06 15:40:12 -0700

    Back to INOUT

--- workspace/popjava/src/popjava/service/POPJavaJobManager.java
+++ workspace/popjava/src/popjava/service/POPJavaJobManager.java
@@ -63,8 +63,8 @@ public class POPJavaJobManager extends POPObject implements JobManagerService {
 	@POPSyncConc(id = 12)
 	public int createObject(POPAccessPoint localservice, String objname,
 		ObjectDescriptionInput od,
-		int howmany, POPAccessPoint[] objcontacts,
-		int howmany2, POPAccessPoint[] remotejobcontacts) {
+		int howmany, @POPParameter(POPParameter.Direction.INOUT) POPAccessPoint[] objcontacts,
+		int howmany2, @POPParameter(POPParameter.Direction.INOUT) POPAccessPoint[] remotejobcontacts) {
 		
 		System.out.println(String.format("%s %s %s %s %s %s %s", localservice, objname, od, howmany, 
 		objcontacts, howmany2, remotejobcontacts));
