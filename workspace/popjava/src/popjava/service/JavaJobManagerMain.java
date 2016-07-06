ef50f98b4d5614bbd2408f39b5891a8c13e743fa
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-05 19:03:27 -0700

    Simply create object

--- workspace/popjava/src/popjava/service/JavaJobManagerMain.java
+++ workspace/popjava/src/popjava/service/JavaJobManagerMain.java
@@ -9,6 +9,7 @@ import popjava.baseobject.AccessPoint;
 import popjava.baseobject.ObjectDescription;
 import popjava.baseobject.POPAccessPoint;
 import popjava.broker.Broker;
+import popjava.combox.ComboxFactory;
 import popjava.combox.ComboxServer;
 import popjava.combox.ComboxSocketFactory;
 import popjava.dataswaper.ObjectDescriptionInput;
@@ -38,45 +39,45 @@ public class JavaJobManagerMain implements Runnable {
 
 		System.out.println("Initialized");
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				ComboxSocketFactory csf = new ComboxSocketFactory();
-				AccessPoint accessPoint = new AccessPoint(AccessPoint.SOCKET_PROTOCOL, AccessPoint.DEFAULT_HOST, POPJobManager.DEFAULT_PORT);
-				ComboxServer cs = csf.createServerCombox(accessPoint, null, Broker.getBroker());
-			}
-		}).start();
+//		new Thread(new Runnable() {
+//			@Override
+//			public void run() {
+//				ComboxFactory cf = new ComboxSocketFactory();
+//				AccessPoint accessPoint = new AccessPoint(AccessPoint.SOCKET_PROTOCOL, AccessPoint.DEFAULT_HOST, POPJobManager.DEFAULT_PORT);
+//				ComboxServer cs = cf.createServerCombox(accessPoint, null, Broker.getBroker());
+//			}
+//		}).start();
 
-		System.out.println("Thread started");
+//		System.out.println("Thread started");
+//
+//		pap = new POPAccessPoint(String.format("%s://%s:%d", AccessPoint.SOCKET_PROTOCOL, AccessPoint.DEFAULT_HOST, POPJobManager.DEFAULT_PORT));
+//
+//		System.out.println("AP Created");
+//		
+//		new Thread(new Runnable() {
+//			@Override
+//			public void run() {
+//				try {
+//					Thread.sleep(10000);
+//					int createObject = jobM.createObject(pap, "What?!", new ObjectDescriptionInput(), 1, new POPAccessPoint[1], 1, new POPAccessPoint[1]);
+//					Thread.sleep(2000);
+//					System.exit(0);
+//				} catch (InterruptedException ex) {
+//					Logger.getLogger(JavaJobManagerMain.class.getName()).log(Level.SEVERE, null, ex);
+//				}
+//			}
+//		}).start();
+		
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException ex) {
+//			Logger.getLogger(JavaJobManagerMain.class.getName()).log(Level.SEVERE, null, ex);
+//		}
+		
+		jobM = PopJava.newActive(POPJavaJobManager.class);
 
-		pap = new POPAccessPoint(String.format("%s://%s:%d", AccessPoint.SOCKET_PROTOCOL, AccessPoint.DEFAULT_HOST, POPJobManager.DEFAULT_PORT));
-
-		System.out.println("AP Created");
 		
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(10000);
-					int createObject = jobM.createObject(pap, "What?!", new ObjectDescriptionInput(), 1, new POPAccessPoint[1], 1, new POPAccessPoint[1]);
-					Thread.sleep(2000);
-					System.exit(0);
-				} catch (InterruptedException ex) {
-					Logger.getLogger(JavaJobManagerMain.class.getName()).log(Level.SEVERE, null, ex);
-				}
-			}
-		}).start();
-		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException ex) {
-			Logger.getLogger(JavaJobManagerMain.class.getName()).log(Level.SEVERE, null, ex);
-		}
-		
-		jobM = PopJava.newActive(POPJavaJobManager.class, pap);
-
-		
-		POPSystem.end();
+//		POPSystem.end();
 	}
 
 	private void start() {
