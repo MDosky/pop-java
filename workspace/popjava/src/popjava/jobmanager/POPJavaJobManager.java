0d0433432731c1326fd39c06129679a9b485982e
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-11 14:17:50 -0700

    Revert "Only local jar"
    This reverts commit 35f0f499f373fb92e75220af105e7e789686fcd5.

--- workspace/popjava/src/popjava/jobmanager/POPJavaJobManager.java
+++ workspace/popjava/src/popjava/jobmanager/POPJavaJobManager.java
@@ -243,7 +243,7 @@ public class POPJavaJobManager extends POPObject implements JobManagerService {
 
 	private static String getPOPCodeFile() {
 
-		String popPath = "";
+		String popPath = POPJavaConfiguration.getClassPath();
 		String popJar = POPJavaConfiguration.getPopJavaJar();
 
 		return String.format(
