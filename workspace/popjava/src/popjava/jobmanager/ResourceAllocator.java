01453e17805ecc7e2c5fb423e45216ea92c9302d
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-26 14:34:47 -0700

    Revert "Guard javaagent revert rest"
    This reverts commit 5734ad2f702b3599b01890242d295b141b9d5370.

--- workspace/popjava/src/popjava/jobmanager/ResourceAllocator.java
+++ workspace/popjava/src/popjava/jobmanager/ResourceAllocator.java
@@ -1,5 +1,8 @@
 package popjava.jobmanager;
 
+import popjava.annotation.POPClass;
+import popjava.annotation.POPSyncConc;
+import popjava.annotation.POPSyncSeq;
 import popjava.dataswaper.ObjectDescriptionInput;
 
 /**
@@ -8,20 +11,26 @@ import popjava.dataswaper.ObjectDescriptionInput;
  *
  * @author Dosky
  */
-public interface ResourceAllocator {
+@POPClass
+public class ResourceAllocator {
 	/**
 	 * This method should return new next Service the object should
 	 * connect to, an OD is given in case we want to handle its 
 	 * parameters
 	 * @param od
 	 * @return 
-	 * */
-	ServiceConnector getNextHost(ObjectDescriptionInput od);
+	 */
+    @POPSyncSeq
+	ServiceConnector getNextHost(ObjectDescriptionInput od) {
+		return null;
+	}
 	
 	/**
 	 * This method register a service for latter use, the JM usually
 	 * call this method
 	 * @param service 
 	 */
-	void registerService(ServiceConnector service);
+    @POPSyncConc
+	void registerService(ServiceConnector service) {
+	}
 }
