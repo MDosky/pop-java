0b1493b8ef285068b4a35d6e15052c0bffc7f79b
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-26 14:34:38 -0700

    Revert "Don't extends POPObj"
    This reverts commit 8a4e47006ce0df847c86b2e28e2e2c9c4d94b507.

--- workspace/popjava/src/popjava/jobmanager/POPJavaJobManager.java
+++ workspace/popjava/src/popjava/jobmanager/POPJavaJobManager.java
@@ -39,7 +39,7 @@ import popjava.util.Util;
  * @see Interface.java regarding the remote object creation
  */
 @POPClass(classId = 99924, deconstructor = false, isDistributable = true)
-public class POPJavaJobManager implements JobManagerService {
+public class POPJavaJobManager extends POPObject implements JobManagerService {
 	
 	private final ResourceAllocator allocator;
 
@@ -302,7 +302,7 @@ public class POPJavaJobManager implements JobManagerService {
 			argvList.add(appString);
 		}
 		// always use this job manager for every object
-		String jobString = String.format("-jobservice=%s", PopJava.getAccessPoint(this).toString());
+		String jobString = String.format("-jobservice=%s", getAccessPoint().toString());
 		argvList.add(jobString);
 		
 		// remove codelocation
@@ -362,10 +362,6 @@ public class POPJavaJobManager implements JobManagerService {
 		}
 		allocateCombox.close();
 		return result;
-	}
-
-	@Override
-	public void exit() {
 	}
 
 }
